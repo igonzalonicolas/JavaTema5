d34a8645b98a4290f6d0aa25cad1c6e06cafcd97
igonzalonicolas <102329396+igonzalonicolas@users.noreply.github.com>
2022-11-22 17:22:19 -0300

    Add files via upload

---
+++ Ejercicio tema 5/src/Coche.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Coche implements CocheCRUD{
+
+    public Coche() {
+
+    }
+
+    @Override
+    public void save(Coche coche) {
+
+    }
+
+    @Override
+    public List<Coche> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Coche coche) {
+
+    }
+}
